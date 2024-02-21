a3a870ab608660431c8945d7d2237d19c40b9b85
Chet Hill <chetdhill@gmail.com>
2024-02-21 16:15:33 -0600

    String Interpolation in Svelte

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -1,11 +1,19 @@
 <script>
 	export let name;
+	export let age;
+	export let occupation;
 </script>
 
 <style>
 	h1 {
-		color: purple;
+		color: green;
+	}
+	h3 {
+		color: blue;
+		font-family: serif;
 	}
 </style>
 
 <h1>Hello {name}!</h1>
+<h3>You are {age} years old today</h3>
+<p>{name} works as a {occupation}</p>
