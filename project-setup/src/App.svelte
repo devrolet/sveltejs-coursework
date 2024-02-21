89da8f073053409ce5f1bd6f4d38b0178887a63e
Chet Hill <chetdhill@gmail.com>
2024-02-21 16:23:21 -0600

    Add increment age function to see real-time changes in SPA

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -2,6 +2,8 @@
 	let name = "NBCU";
 	let age = 101;
 	let industry = "Media Organization";
+
+	let incrementAge = () => age += 1;
 </script>
 
 <style>
@@ -17,3 +19,5 @@
 <h1>Hello {name}!</h1>
 <h3>You are {age} years old today</h3>
 <p>{name} is in the {industry} industry</p>
+<!-- The {} are required, "" are not, enter name of function w/o the () to execute -->
+<button on:click="{incrementAge}">Change Age</button> 
