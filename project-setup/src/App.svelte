a5dfff622e9c91db27a72a005fcef9d0edf3aa92
Chet Hill <chetdhill@gmail.com>
2024-02-22 08:17:39 -0600

    Reactive Variables in SvelteJS

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -1,9 +1,14 @@
 <script>
-	let name = "NBCU";
-	let age = 101;
-	let industry = "Media Organization";
+	let name = "Chet Hill";
+	let age = 46;
+	let industry = "Computer Programming";
+
+	// Labeled Statements anyword: action
+	$: uppercaseName = name.toUpperCase();
 
 	let incrementAge = () => age += 1;
+
+	let changeName = () => name = "Malichi Hill";
 </script>
 
 <style>
@@ -16,8 +21,9 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {uppercaseName}!</h1>
 <h3>You are {age} years old today</h3>
 <p>{name} is in the {industry} industry</p>
 <!-- The {} are required, "" are not, enter name of function w/o the () to execute -->
 <button on:click="{incrementAge}">Change Age</button> 
+<button on:click="{changeName}">Change Name</button>
