f75dcfffcd5f3779b6a2de497273eb97afa80d90
Chet Hill <chetdhill@gmail.com>
2024-02-22 13:22:41 -0600

    Final assignment change

--- assignment-1/src/App.svelte
+++ assignment-1/src/App.svelte
@@ -12,7 +12,7 @@
   <li><strike>Add an input field that allows users to enter a course goal.</strike></li>
   <li><strike>Output the user input in a h1 tag.</strike></li>
   <li>
-    Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.
+    <strike>Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.</strike>
   </li>
   <li>
     <strike>Put the h1 tag + output into a separate component to which you pass the user input</strike>
