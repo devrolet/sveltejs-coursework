cf9df6febf1774b658e2b9071bb9a69d211b2861
Chet Hill <chetdhill@gmail.com>
2024-02-22 13:21:57 -0600

    Complete Assignment 1

--- assignment-1/src/App.svelte
+++ assignment-1/src/App.svelte
@@ -1,14 +1,25 @@
+<script>
+  import Goal from "./Goal.svelte";
+
+  let courseGoal = "";
+
+</script>
 <h1>Assignment</h1>
 
 <p>Solve these tasks.</p>
 
 <ol>
-  <li>Add an input field that allows users to enter a course goal.</li>
-  <li>Output the user input in a h1 tag.</li>
+  <li><strike>Add an input field that allows users to enter a course goal.</strike></li>
+  <li><strike>Output the user input in a h1 tag.</strike></li>
   <li>
     Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.
   </li>
   <li>
-    Put the h1 tag + output into a separate component to which you pass the user input
+    <strike>Put the h1 tag + output into a separate component to which you pass the user input</strike>
   </li>
 </ol>
+
+<label for="course-goal">What is your goal for this course?</label>
+<input id="course-goal" type="text" bind:value="{courseGoal}" />
+
+<Goal goal={courseGoal}/>
