c2f8abf0acd40489b6efc0d25f9a1d04d4dd7898
Chet Hill <chetdhill@gmail.com>
2024-03-05 10:41:05 -0600

    Add dummy data and create empty MeetupItem component

--- svelte-course-project/public/build/bundle.js
+++ svelte-course-project/public/build/bundle.js
@@ -495,13 +495,35 @@ var app = (function () {
     function instance($$self, $$props, $$invalidate) {
     	let { $$slots: slots = {}, $$scope } = $$props;
     	validate_slots('App', slots, []);
+
+    	const meetups = [
+    		{
+    			id: 'm1',
+    			title: 'Cooking Bootcamp',
+    			subtitle: "Learn to cook in 2 hours",
+    			description: 'In this meetup, we have master chefs and culinary experts to help guide you.',
+    			imageUrl: 'https://www.pexels.com/photo/spices-on-bowl-near-knife-on-chopping-boad-33242/',
+    			address: '1930 Studebaker Road, Atlanta, GA 30303',
+    			contactEmail: 'cookingboots@test.com'
+    		},
+    		{
+    			id: 'm2',
+    			title: 'Music Production for Beginners',
+    			subtitle: "Learn how to make music in your bedroom",
+    			description: 'In this meetup, you\'ll learn the basics of music production',
+    			imageUrl: 'https://www.pexels.com/photo/photo-of-headphone-and-amplifier-2426085/',
+    			address: '8130 Olympic Way, Atlanta, GA 30303',
+    			contactEmail: 'beatmakers@test.com'
+    		}
+    	];
+
     	const writable_props = [];
 
     	Object.keys($$props).forEach(key => {
     		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== '$$' && key !== 'slot') console.warn(`<App> was created with unknown prop '${key}'`);
     	});
 
-    	$$self.$capture_state = () => ({ Header });
+    	$$self.$capture_state = () => ({ Header, meetups });
     	return [];
     }
 
