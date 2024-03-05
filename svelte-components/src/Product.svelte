5784e21325447300e9ca757fb7fc165471ee7cdc
Chet Hill <chetdhill@gmail.com>
2024-03-05 16:52:19 -0600

    Event forwarding in Svelte

---
+++ svelte-components/src/Product.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let productTitle;
+</script>
+
+<article>
+    <h1>{productTitle}</h1>
+    <!-- Event forwarded to another component (App in this case) -->
+    <button on:click>Add to Cart</button>
+    <button>Delete</button>
+</article>
