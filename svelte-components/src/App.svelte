aa1db33f9ddd205d9aeb4b1d146ba50b349eb164
Chet Hill <chetdhill@gmail.com>
2024-03-08 13:18:08 -0600

    Using spread props & default props: Spread props

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -1,6 +1,19 @@
 <script>
 	import Product from "./Product.svelte";
 
+	let products = [
+		{
+			id: 'p1',
+			title: 'A Book About Everything',
+			price: 9.99
+		},
+		{
+			id: 'p2',
+			title: 'A Book About Nothing',
+			price: 19.99
+		}
+	]
+
 	// Extract event data to pass data up the chain
 	function addToCart(event) {
 		console.log(event);
@@ -11,8 +24,10 @@
 	}
 </script>
 <!-- Call the functions to extract data from detail property or obj -->
+{#each products as product}
+<!-- Props could be called by their name for example product.title or ...product will use spread operator to show all products-->
 <Product 
-	productTitle="A Book" 
+	{...product} 
 	on:add-to-cart={addToCart}
-	on:delete={deleteProduct} 
-/>
+	on:delete={deleteProduct} />
+{/each}
