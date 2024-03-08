77babb89b111c671efe9f0c7dba457b73dab2d5e
Chet Hill <chetdhill@gmail.com>
2024-03-08 12:20:19 -0600

    Extract event data, send data from child compo to parent

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -1,8 +1,18 @@
 <script>
 	import Product from "./Product.svelte";
+
+	// Extract event data to pass data up the chain
+	function addToCart(event) {
+		console.log(event);
+	}
+
+	function deleteProduct(event) {
+		console.log(event.detail);
+	}
 </script>
-<!-- Event forwarded to App component to execute on:click -->
+<!-- Call the functions to extract data from detail property or obj -->
 <Product 
 	productTitle="A Book" 
-	on:add-to-cart={() => alert('Add to cart')}
-	on:delete={() => alert('Delete')} />
+	on:add-to-cart={addToCart}
+	on:delete={deleteProduct} 
+/>
