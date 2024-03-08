80892a954a676dd9809d102f128527bc8a64545e
Chet Hill <chetdhill@gmail.com>
2024-03-08 12:01:32 -0600

    Update Event forwarding comments

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -1,5 +1,5 @@
 <script>
 	import Product from "./Product.svelte";
 </script>
-
+<!-- Event forwarded to App component to execute on:click -->
 <Product productTitle="A Book" on:click={() => alert('Clicked')} />
