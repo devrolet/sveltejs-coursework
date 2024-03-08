61eec19124d31be6fe15a47e5e1fb724e64894bf
Chet Hill <chetdhill@gmail.com>
2024-03-08 12:11:55 -0600

    Svelte: Emitting custom events

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -2,4 +2,7 @@
 	import Product from "./Product.svelte";
 </script>
 <!-- Event forwarded to App component to execute on:click -->
-<Product productTitle="A Book" on:click={() => alert('Clicked')} />
+<Product 
+	productTitle="A Book" 
+	on:add-to-cart={() => alert('Add to cart')}
+	on:delete={() => alert('Delete')} />
--- svelte-components/src/Product.svelte
+++ svelte-components/src/Product.svelte
@@ -1,10 +1,18 @@
 <script>
+    import { createEventDispatcher } from 'svelte';
+
     export let productTitle;
+
+    const dispatch = createEventDispatcher();
+
+    function addToCart() {
+        dispatch('add-to-cart', {id: 'p1'});
+    }
 </script>
 
 <article>
     <h1>{productTitle}</h1>
     <!-- Event forwarded to another component (App in this case) -->
-    <button on:click>Add to Cart</button>
-    <button>Delete</button>
+    <button on:click={addToCart}>Add to Cart</button>
+    <button on:click="{() => {dispatch('delete', 'p1')}}">Delete</button>
 </article>
