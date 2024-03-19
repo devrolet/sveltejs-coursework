f82c3c7278c6f9e3e30d2dd7e621c35e39e1a51b
Chet Hill <chetdhill@gmail.com>
2024-03-19 16:04:35 -0500

    Communication with custom events

--- svelte-course-project/src/App.svelte
+++ svelte-course-project/src/App.svelte
@@ -19,7 +19,8 @@
             description: 'In this meetup, we have master chefs and culinary experts to help guide you.',
             imageUrl: 'https://www.pexels.com/photo/spices-on-bowl-near-knife-on-chopping-boad-33242/',
             address: '1930 Studebaker Road, Atlanta, GA 30303',
-            contactEmail: 'cookingboots@test.com'
+            contactEmail: 'cookingboots@test.com',
+            isFavorite: false
         },
         {
             id: 'm2',
@@ -28,7 +29,8 @@
             description: 'In this meetup, you\'ll learn the basics of music production',
             imageUrl: 'https://www.pexels.com/photo/photo-of-headphone-and-amplifier-2426085/',
             address: '8130 Olympic Way, Atlanta, GA 30303',
-            contactEmail: 'beatmakers@test.com'
+            contactEmail: 'beatmakers@test.com',
+            isFavorite: false
         }
     ]
 
@@ -45,6 +47,16 @@
 
         meetups = [newMeetup, ...meetups];
         console.log('Meetup Added Successfully');
+    }
+
+    let toggleFavorite = event => {
+        const id = event.detail;
+        const updatedMeetup = { ...meetups.find(m => m.id === id) };
+        updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+        const meetupIndex = meetups.findIndex(m => m.id === id);
+        const updatedMeetups = [...meetups];
+        updatedMeetups[meetupIndex] = updatedMeetup;
+        meetups = updatedMeetups;
     }
 </script>
 <style>
@@ -97,5 +109,5 @@
             on:input={event => (description = event.target.value)} />
         <Button type="submit" caption="Save" />
     </form>
-    <MeetupGrid {meetups} />
+    <MeetupGrid {meetups} on:toggle-favorite="{toggleFavorite}" />
 </main>
--- svelte-course-project/src/UI/Button.svelte
+++ svelte-course-project/src/UI/Button.svelte
@@ -80,5 +80,5 @@
 {#if href}
     <a {href}>{caption}</a>
 {:else}
-    <button class="{mode}" {type}>{caption}</button>
+    <button class="{mode}" {type} on:click>{caption}</button>
 {/if}
