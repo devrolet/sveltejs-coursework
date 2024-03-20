eabd123a3f84dc1e63b6d2bb68c2a9246d636d01
Chet Hill <chetdhill@gmail.com>
2024-03-20 15:19:54 -0500

    Add new svelte files for binding

--- svelte-binding/src/App.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    console.log('This works');
-</script>
-
-<h1>Bindings & Forms</h1>
---
+++ svelte-binding2/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+    let val = 'Malichi';
+
+    let setValue = event => val = event.target.setValue;
+</script>
+
+<h1>Bindings & Forms</h1>
+<input type="text" value={val} on:input={setValue}>
