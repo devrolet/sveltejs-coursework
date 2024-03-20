268105c367a1b7799df2753a045c8b0ccc63ecb6
Chet Hill <chetdhill@gmail.com>
2024-03-20 15:26:53 -0500

    Two-Way Binding in Svelte Refresher

--- svelte-binding2/src/App.svelte
+++ svelte-binding2/src/App.svelte
@@ -1,8 +1,9 @@
 <script>
     let val = 'Malichi';
-
+	$: console.log(val);
+	
     let setValue = event => val = event.target.setValue;
 </script>
 
 <h1>Bindings & Forms</h1>
-<input type="text" value={val} on:input={setValue}>
+<input type="text" bind:value={val}>
