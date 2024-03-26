62b24c11ec61df0b2683755ce44e2deda954a2ca
Chet Hill <chetdhill@gmail.com>
2024-03-26 16:57:51 -0500

    Prepare transition example

--- svelte-motion-animation/src/App.svelte
+++ svelte-motion-animation/src/App.svelte
@@ -5,6 +5,8 @@
 
     import Spring from './Spring.svelte';
 
+    let boxInput;
+
     const progress = tweened(0, {
         delay: 0,
         duration: 700,
@@ -13,10 +15,30 @@
     setTimeout(() => {
         progress.set(0.5);
     }, 1500);
+
+    let boxes = [];
+
+    let addBox = () => {
+        boxes = [...boxes, boxInput.value];
+    }
 </script>
-
+<style>
+    div {
+        width: 10rem;
+        height: 10rem;
+        background: #ccc;
+        margin: 1rem;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+        border-radius: 5px;
+        padding: 1rem;
+    }
+</style>
 <!-- <progress value={$progress}></progress> -->
-<Spring />
-
+<!-- <Spring /> -->
+<input type="text" bind:this={boxInput} />
+<button on:click={addBox}>Add</button>
+{#each boxes as box (box)}
+    <div>{box}</div>
+{/each}
 
 
