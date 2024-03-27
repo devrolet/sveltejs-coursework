d7639fc7b458ec66c96c3296126231efc4082b94
Chet Hill <chetdhill@gmail.com>
2024-03-27 11:32:01 -0500

    Fetching and Transforming Data

--- svelte-course-project/src/Meetups/meetups-store.js
+++ svelte-course-project/src/Meetups/meetups-store.js
@@ -1,30 +1,12 @@
 import { writable } from 'svelte/store';
 
-const meetups = writable([
-    {
-        id: 'm1',
-        title: 'Cooking Bootcamp',
-        subtitle: "Learn to cook in 2 hours",
-        description: 'In this meetup, we have master chefs and culinary experts to help guide you.',
-        imageUrl: 'https://www.pexels.com/photo/spices-on-bowl-near-knife-on-chopping-boad-33242.jpg/',
-        address: '1930 Studebaker Road, Atlanta, GA 30303',
-        contactEmail: 'cookingboots@test.com',
-        isFavorite: false
-    },
-    {
-        id: 'm2',
-        title: 'Music Production for Beginners',
-        subtitle: "Learn how to make music in your bedroom",
-        description: 'In this meetup, you\'ll learn the basics of music production',
-        imageUrl: 'https://www.pexels.com/photo/photo-of-headphone-and-amplifier-2426085.jpg/',
-        address: '8130 Olympic Way, Atlanta, GA 30303',
-        contactEmail: 'beatmakers@test.com',
-        isFavorite: false
-    }
-]);
+const meetups = writable([]);
 
 const customMeetupsStore = {
     subscribe: meetups.subscribe,
+    setMeetups: (meetupArray) => {
+        meetups.set(meetupArray);
+    },
     addMeetup: (meetupData) => {
         const newMeetup = {
             ...meetupData
