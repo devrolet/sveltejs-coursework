e0697bcec4ef52d1e60dba55bcbf9a0d36d11373
Chet Hill <chetdhill@gmail.com>
2024-03-27 11:18:10 -0500

    Storing meetups on a server

--- svelte-course-project/src/Meetups/meetups-store.js
+++ svelte-course-project/src/Meetups/meetups-store.js
@@ -6,7 +6,7 @@ const meetups = writable([
         title: 'Cooking Bootcamp',
         subtitle: "Learn to cook in 2 hours",
         description: 'In this meetup, we have master chefs and culinary experts to help guide you.',
-        imageUrl: 'https://www.pexels.com/photo/spices-on-bowl-near-knife-on-chopping-boad-33242/',
+        imageUrl: 'https://www.pexels.com/photo/spices-on-bowl-near-knife-on-chopping-boad-33242.jpg/',
         address: '1930 Studebaker Road, Atlanta, GA 30303',
         contactEmail: 'cookingboots@test.com',
         isFavorite: false
@@ -16,7 +16,7 @@ const meetups = writable([
         title: 'Music Production for Beginners',
         subtitle: "Learn how to make music in your bedroom",
         description: 'In this meetup, you\'ll learn the basics of music production',
-        imageUrl: 'https://www.pexels.com/photo/photo-of-headphone-and-amplifier-2426085/',
+        imageUrl: 'https://www.pexels.com/photo/photo-of-headphone-and-amplifier-2426085.jpg/',
         address: '8130 Olympic Way, Atlanta, GA 30303',
         contactEmail: 'beatmakers@test.com',
         isFavorite: false
@@ -27,9 +27,7 @@ const customMeetupsStore = {
     subscribe: meetups.subscribe,
     addMeetup: (meetupData) => {
         const newMeetup = {
-            ...meetupData,
-            id: Math.random().toString(),
-            isFavorite: false
+            ...meetupData
         }
         meetups.update(items => {
             return [newMeetup, ...items];
