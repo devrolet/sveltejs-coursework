7a8bdc118895d89724a93ed823611d1059070892
Chet Hill <chetdhill@gmail.com>
2024-03-27 15:06:56 -0500

    Toggling between different components (the inefficient way)

---
+++ svelte-special/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Product from './Product.svelte';
+    import CartItem from './CartItems.svelte';
+  import CartItems from './CartItems.svelte';
+
+    let showProduct = true;
+
+    let toggle = () => {
+        showProduct = !showProduct;
+    }
+</script>
+
+<button on:click={toggle}>Toggle Display</button>
+{#if showProduct}
+    <Product  title="Test Product" id="p1" />
+{:else}
+    <CartItems title="Another Product" id="p2" />
+{/if}
---
+++ svelte-special/src/CartItems.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let title;
+    export let id;
+</script>
+<h1>{id} | {title}</h1>
+<button>Remove from Cart</button>
